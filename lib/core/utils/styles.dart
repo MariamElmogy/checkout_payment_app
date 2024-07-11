a84dea010417da70b45bd13fb06ef297cfe72939
MariamElmogy <mariamelmogy3@gmail.com>
2024-07-11 14:41:01 +0300

    - build app bar - create styles file

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static TextStyle style25 = const TextStyle(
+    color: Colors.black,
+    fontSize: 25,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Inter',
+    height: 0,
+  );
+  static TextStyle style18 = const TextStyle(
+    color: Colors.black,
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Inter',
+  );
+  static TextStyle styleBold18 = const TextStyle(
+    color: Colors.black,
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Inter',
+  );
+
+  static TextStyle style24 = const TextStyle(
+    color: Colors.black,
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Inter',
+    height: 0,
+  );
+
+  static TextStyle style20 =  TextStyle(
+    color: Colors.black.withOpacity(0.800000011920929),
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Inter',
+  );
+}
---
+++ lib/feature/checkout/presentation/my_cart_view.dart
@@ -0,0 +1,25 @@
+import 'package:checkout_payment_ui/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class MyCartView extends StatelessWidget {
+  const MyCartView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: Text(
+          'My Cart',
+          textAlign: TextAlign.center,
+          style: Styles.style25,
+        ),
+        leading: Center(
+          child: SvgPicture.asset('assets/images/arrow.svg'),
+        ),
+      ),
+    );
+  }
+}
